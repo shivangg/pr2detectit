c286f2ad3901eca07eaf5ca4034fe75842a308f8
shivangg <xshivangx@gmail.com>
2017-11-23 13:02:05 -0800

    test_world2 successful output

--- pr2_robot/scripts/object_recog.py
+++ pr2_robot/scripts/object_recog.py
@@ -247,7 +247,7 @@ def pr2_mover(detected_objects):
 
 
     # TODO: Parse parameters into individual variables
-    test_scene_num.data = 3
+    test_scene_num.data = 2
 
     # TODO: Rotate PR2 in place to capture side tables for the collision map
 
